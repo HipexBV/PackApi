5d1bf636718834f11f20eea829d35f794d2bfcb1
Freek Gruntjes <fgruntjes@emico.nl>
2018-06-04 10:11:25 +0200

    Added domain search by server

--- src/HipexPackApi/Repository/DomainRepository.php
+++ src/HipexPackApi/Repository/DomainRepository.php
@@ -8,6 +8,7 @@ namespace HipexPackApi\Repository;
 
 use HipexPackApi\Exception\EntityNotFoundException;
 use HipexPackApi\Exception\ResultNonUniqueException;
+use HipexPackApi\Generated\Schema\Input\FilterInput;
 use HipexPackApi\Generated\Schema\Type;
 use HipexPackApi\Generated\Schema\Query;
 use HipexPackApi\Schema\BaseQuery;
@@ -16,14 +17,24 @@ class DomainRepository extends AbstractRepository
 {
     /**
      * @param string $domainName
+     * @param string|null $server
      * @return Type\Domain
-     * @throws ResultNonUniqueException
-     * @throws EntityNotFoundException
      */
-    public function findByDomainName(string $domainName): Type\Domain
+    public function findByDomainName(string $domainName, string $server = null): Type\Domain
     {
+        if ($server === null) {
+            /** @noinspection PhpIncompatibleReturnTypeInspection */
+            return $this->findOneByField('domain', $domainName);
+        }
+
+        $filter = new FilterInput();
+        $filter->setOperator('$and');
+        $filter->setChildren([
+            (new FilterInput)->setField('domain')->setValue($domainName),
+            (new FilterInput)->setField('server.name')->setValue($server),
+        ]);
         /** @noinspection PhpIncompatibleReturnTypeInspection */
-        return $this->findOneByField('domain', $domainName);
+        return $this->findOne($filter);
     }
 
     /**
