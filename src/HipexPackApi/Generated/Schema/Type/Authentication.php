94dc483e8476727524e06f9c75b38b5fb501f65d
Hipex <info@hipex.io>
2019-01-02 09:08:27 +0100

    Automated GraphQL update to version 1.36.0

--- src/HipexPackApi/Generated/Schema/Type/Authentication.php
+++ src/HipexPackApi/Generated/Schema/Type/Authentication.php
@@ -2,7 +2,7 @@
 
 /**
  * @author Hipex B.V. <info@hipex.io>
- * @copyright (c) Hipex B.V. 2015-18
+ * @copyright (c) Hipex B.V. 2015-19
  * @see https://service.hipex.io/hipex-pack-api/docs/
  * File generated from https://service.hipex.io/hipex-pack-api GraphQL endpoint.
  */
