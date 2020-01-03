9fad7970d0a3abdd2b5cd8930cb69848b0c2c5eb
Hipex <info@hipex.io>
2020-01-03 13:05:37 +0100

    Automated GraphQL update to version 1.42.0

--- src/HipexPackApi/Generated/Schema/Type/PhpVersionEnum.php
+++ src/HipexPackApi/Generated/Schema/Type/PhpVersionEnum.php
@@ -2,7 +2,7 @@
 
 /**
  * @author Hipex B.V. <info@hipex.io>
- * @copyright (c) Hipex B.V. 2015-19
+ * @copyright (c) Hipex B.V. 2015-20
  * @see https://service.hipex.io/hipex-pack-api/docs/
  * File generated from https://service.hipex.io/hipex-pack-api GraphQL endpoint.
  */
@@ -15,15 +15,15 @@ namespace HipexPackApi\Generated\Schema\Type;
  */
 class PhpVersionEnum extends \HipexPackApi\Schema\BaseType
 {
-	/** PHP 7.4 - BETA */
+	/** PHP 7.4 */
 	const VALUE_PHP74 = 'php74';
 	/** PHP 7.3 */
 	const VALUE_PHP73 = 'php73';
 	/** PHP 7.2 */
 	const VALUE_PHP72 = 'php72';
-	/** PHP 7.1 */
+	/** PHP 7.1 - DEPRECATED */
 	const VALUE_PHP71 = 'php71';
-	/** PHP 7.0 - DEPRECATED */
+	/** PHP 7.0 - NOT SUPPORTED */
 	const VALUE_PHP70 = 'php70';
 	/** PHP 5.6 - NOT SUPPORTED */
 	const VALUE_PHP56 = 'php56';
